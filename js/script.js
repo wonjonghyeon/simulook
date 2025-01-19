0e50b738eb108bcbeb0d5b7ab22226d53e3350bd
wonjonghyeon <37322287+wonjonghyeon@users.noreply.github.com>
2025-01-19 21:44:04 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,32 +1,25 @@
 const slider = document.querySelector('.slider');
-const images = document.querySelectorAll('.slide');
+const slides = document.querySelectorAll('.slide');
 
-let currentIndex = 0;
+let slideIndex = 0;
 
 // 초기 이미지 활성화
-images[currentIndex].classList.add('active');
+slides[slideIndex].classList.add('active');
 
 
 // 이미지 슬라이드 함수
-function slide(direction) {
-  const currentImage = images[currentIndex];
-  let nextIndex = currentIndex + direction;
-
-  // 이미지 인덱스를 0과 2 사이로 유지
-  if (nextIndex < 0) {
-    nextIndex = images.length - 1;
-  } else if (nextIndex >= images.length) {
-    nextIndex = 0;
+function showSlide(n) {
+  slides[slideIndex].classList.remove('active');
+  slideIndex += n;
+  
+  if(slideIndex >= slides.length){
+      slideIndex = 0
   }
-
-
-  // 현재 이미지 숨기고 다음 이미지 보이
-  images.forEach(image => 
-                image.classList.remove('active'));
-  images[currentIndex].classList.add('active');
-       
+  if(slideIndex < 0){
+      slideIndex = slides.length - 1
+  }
+  slides[slideIndex].classList.add('active');
     
-  currentIndex = nextIndex;
 }
 
 // 터치 이벤트 처리
@@ -40,9 +33,9 @@ slider.addEventListener('touchstart', (event) => {
 
     // 왼쪽으로 스와이프하면 다음 이미지, 오른쪽으로 스와이프하면 이전 이미지
     if (diffX > 50) {
-      slide(-1);
+      showSlide(-1);
     } else if (diffX < -50) {
-      slide(1);
+      showSlide(1);
     }
   });
 });
